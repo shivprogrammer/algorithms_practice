1e59e65e4f59bcd11bda52c777c8b9d055c11df1
Shiv <shiv.unadkat@gmail.com>
2017-04-12 16:40:46 -0700

    success on dat palindrome

--- stacksNQue/stacksQs-q1.js
+++ stacksNQue/stacksQs-q1.js
@@ -1,1 +1,3 @@
 // Write Code for the Towers of Hanoi
+
+I believe that we have already written the code for this in the class, essentially all you need to do is a recursive call between the first and second platforms after the bottom rung of the 
--- strings/strings-q3.js
+++ strings/strings-q3.js
@@ -1,7 +1,24 @@
-HW2: Strings Q3: Test Palindromicity for string
+// HW2: Strings Q3: Test Palindromicity for string
+//
+// Given,
+// andna => true
+// ana => true
+// nn => true
+// tgh =>false
 
-Given,
-andna => true
-ana => true
-nn => true
-tgh =>false
+function palindrome(string) {
+  for (var i = 0; i < string.length/2; i++) {
+    console.log(string[i]);
+    if (string[i] !== string[string.length - 1 - i]) {
+      console.log(string[i]);
+      return false;
+    }
+  }
+  return true;
+}
+
+// O(N/2) time complexity and NO ADDITIONAL SPACE COMPLEXITY
+
+console.log(palindrome('totototototototototototootototototot')); // return false;
+console.log(palindrome('bananab')); // return true;
+console.log(palindrome('abcd')); // return false;
