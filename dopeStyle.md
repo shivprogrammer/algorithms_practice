c246a073e70975f2ca82d541f9a04244f003e3a5
Shiv <shiv.unadkat@gmail.com>
2017-04-12 15:30:32 -0700

    successfully implemented with passing tests and O(n) time complexity the run length algorithm, added in various packages that are useful to add in the aesthetic of the atom interface

---
+++ dopeStyle.md
@@ -0,0 +1,7 @@
+color-picker
+activate-power-mode
+emmet
+eslint
+open in browser
+seti icons
+themes -- neon syntax
--- strings/strings-q2.js
+++ strings/strings-q2.js
@@ -1,5 +1,29 @@
-Strings Q2: Implement run length encoding
+// Strings Q2: Implement run length encoding
 
-<input> => output
-aaaabcccaa => 4a1b3c2a
-eeeffffee => 3e4f2e
+// <input> => output
+// aaaabcccaa => 4a1b3c2a
+// eeeffffee => 3e4f2e
+
+function runLength(string) {
+  var output = [];
+  var counter = 1;
+  if (string === '' || string === null) {
+    return string;
+  }
+  for (var i = 0; i < string.length; i++){
+    if(string[i] === string[i+1]){
+    counter++;
+    output.push(counter + string[i]);
+    }
+    if(string[i] !== string[i+1])
+    counter = 1;
+  }
+  return output.join('');
+}
+
+// O(N) time complexity
+// O(N) space complexity for output variable and then an additional one for the variable for the counter as well.
+
+runLength('aabbcccddbb'); // expect 2a2b3c2d2b
+runLength('abcabc'); // expect 1a1b1c1a1b1c
+runLength('aaaaaba'); // expect 5a1b1a
