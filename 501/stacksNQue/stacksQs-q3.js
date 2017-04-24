0b61f2f2a35eda592ff635bb72c589cf4d5fefde
shivprogrammer <shiv.unadkat@gmail.com>
2017-04-24 16:33:47 -0700

    added in max int stack question that was solved with Thomas

--- 501/stacksNQue/stacksQs-q3.js
+++ 501/stacksNQue/stacksQs-q3.js
@@ -1,5 +1,34 @@
-HW2: Stacks and Queue Q3: Stack with Max API
+// HW2: Stacks and Queue Q3: Stack with Max API
+// Have an api method in your stack class that returns the Max Integer Value in it.
 
-Have an api method in your stack class that returns the Max Integer Value in it.
+// this was solved with Thomas
 
-// THIS PROBLEM HAS BEEN DONE, JUST WRITE THE ANSWERS FROM THE CODE THAT WAS DONE WITH THOMAS
+function Stack() {
+}
+
+var stack1 = new Stack();
+var maxStack = new Stack();
+
+Stack.prototype.maxInt() {
+  return maxStack.peek();
+}
+
+stack1.prototype.pop() {
+  if (maxStack.pop() === stack1.pop()) {
+    maxStack.pop();
+  }
+  else {
+    stack1.pop();
+  }
+}
+
+Stack.prototype.push(val) {
+  if (maxStack.isEmpty()) {
+    maxStack.push(val);
+  }
+  if (maxStack.peek() <= val) {
+    maxStack.push(val);
+    stack1.push(val);
+  }
+  stack1.push(val);
+}
