fd1725ae0e52d16d05b5322d55db2eeba89cd504
shivprogrammer <shiv.unadkat@gmail.com>
2017-04-27 16:57:26 -0700

    GOT THAT SHIT TO WORK SONNNNNNNNNNNNN FUCK YES

--- codewars/array-diff.js
+++ codewars/array-diff.js
@@ -6,13 +6,13 @@
 // If a value is present in b, all of its occurrences must be removed from the other:
 // difference([1,2,2,2,3],[2]) == [1,3]
 
-Array.prototype.diff = function(a) {
-  return this.filter(function(i) {
-    return a.indexOf(i) < 0;
+function array_diff(a, b) {
+  return a.filter(function(i) {
+    return b.indexOf(i) < 0;
   })
 }
 
 var array1 = [1, 2, 2, 4, 4, 3, 5, 6];
 var array2 = [3, 5];
 
-console.log(array1.diff(array2)); // return [1, 2, 2, 4, 4, 6];
+console.log(arrayDiff(array1, array2)); // return [1, 2, 2, 4, 4, 6];
