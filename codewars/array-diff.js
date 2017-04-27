ff5fc9cd010f3706e6596ce0cc9a50063e266374
shivprogrammer <shiv.unadkat@gmail.com>
2017-04-27 16:51:21 -0700

    the array diff problem from code wars is working successfully but I really don't know why. Will have to keep playing with it

---
+++ codewars/array-diff.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Your goal in this kata is to implement an difference function, which subtracts one list from another.
+// It should remove all values from list a, which are present in list b.
+// difference([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+// difference([1,2,2,2,3],[2]) == [1,3]
+
+Array.prototype.diff = function(a) {
+  return this.filter(function(i) {
+    return a.indexOf(i) < 0;
+  })
+}
+
+var array1 = [1, 2, 2, 4, 4, 3, 5, 6];
+var array2 = [3, 5];
+
+console.log(array1.diff(array2)); // return [1, 2, 2, 4, 4, 6];
