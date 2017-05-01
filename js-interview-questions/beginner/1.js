47405c7d250afe8b7f1c65453c996e166c1f80d9
shivprogrammer <shiv.unadkat@gmail.com>
2017-04-30 23:56:39 -0700

    more efficient time complexity for the isPrime algorithm

--- js-interview-questions/beginner/1.js
+++ js-interview-questions/beginner/1.js
@@ -16,7 +16,24 @@ console.log(isPrime(100)); // return false
 console.log(isPrime(27)); // return false
 console.log(isPrime(11)); // return false
 
-// Time complexity is O(N)
+// Time complexity - O(N)
+
+function isPrime2(num) {
+  for (let i = 2, j = Math.sqrt(num); i <= j; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPrime2(13)); // return true
+console.log(isPrime2(100)); // return false
+console.log(isPrime2(27)); // return false
+console.log(isPrime2(11)); // return false
+
+// This solution is more efficient but also quite interesting because the reason behind the usefulness of this goes to arithmetic and understanding the mathematics behind not need to searching all the way until the number itself.
+// Time complexity - O(sqrt(N))
 
 // describe('isPrime module', function() {
 //
