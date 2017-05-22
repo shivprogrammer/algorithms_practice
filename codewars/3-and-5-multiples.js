329a5750ece72271827b2a11fbd39e90c2c3ded3
shivprogrammer <shiv.unadkat@gmail.com>
2017-05-21 23:04:52 -0700

    issue with 3s and 5s multiple, but getting solved

--- codewars/3-and-5-multiples.js
+++ codewars/3-and-5-multiples.js
@@ -11,17 +11,18 @@ Note: If the number is a multiple of both 3 and 5, only count it once.
 function solution(number) {
   var threes = Math.floor((number - 1) / 3);
   var fives = Math.floor((number - 1) / 5);
+  var multiplesArray = [];
   var num3 = 3;
   var num5 = 5;
   var result = 0;
 
   for (let i = 0; i < threes; i++) {
-    result += num3;
+    multiplesArray.push(num3);
     num3 += 3;
   }
 
   for (let j = 0; j < fives; j++) {
-    result += num5;
+    multiplesArray.push(num5);
     num5 += 5;
   }
 
@@ -32,6 +33,13 @@ describe('Code Wars 3 and 5 multiples questions', function() {
   describe('With an input of 10', function() {
     it('should return 23', done => {
       expect(solution(10)).to.equal(23);
+      done();
+    })
+  })
+
+  describe('With an input of 20', function() {
+    it('should return 78', done => {
+      expect(solution(20)).to.equal(78);
       done();
     })
   })
