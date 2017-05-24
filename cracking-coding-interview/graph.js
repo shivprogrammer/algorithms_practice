1a3181868db230a19e964ff6a8e6ea11600f8892
shivprogrammer <shiv.unadkat@gmail.com>
2017-05-23 23:17:50 -0700

    successfully added ability to add an edge to nodes in a graph

--- cracking-coding-interview/graph.js
+++ cracking-coding-interview/graph.js
@@ -3,3 +3,13 @@
 var Graph = function() {
   this.nodes = {};
 }
+
+Graph.prototype.addEdge = function(node, edge) {
+  if (this.nodes[node] === undefined) {
+    return 'node does not exist';
+  } else if (this.node[node][edge]) {
+    return `edge from ${node}-${edge} already exists`;
+  } else {
+    this.nodes[node][edge] = true
+  }
+}
