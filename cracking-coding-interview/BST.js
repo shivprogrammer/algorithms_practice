1dd4a2096d3070c8895a3798c9210d0fe54072d1
shivprogrammer <shiv.unadkat@gmail.com>
2017-05-28 23:34:44 -0700

    BST insert

---
+++ cracking-coding-interview/BST.js
@@ -0,0 +1,24 @@
+function BST(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+BST.prototype.insert = function(value) {
+  if (value < this.value) {
+    if (this.left === null) {
+      this.left = new BST(value);
+    }
+    else  {
+      this.left.insert(value);
+    }
+  }
+  else {
+    if (this.right === null) {
+      this.right = new BST(value);
+    }
+    else {
+      this.right.insert(value);
+    }
+  }
+}
