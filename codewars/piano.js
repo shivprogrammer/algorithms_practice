3f32ae6823e071e7bb94be236c5955924cdd04ef
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-11 23:39:56 -0700

    progress on the piano

--- codewars/piano.js
+++ codewars/piano.js
@@ -1,3 +1,5 @@
 'use strict';
 
 const expect = require('chai').expect;
+
+const piano = ['white', 'black', 'white', 'white', 'black', 'white', 'black', 'white', 'white', 'black', 'white', 'black', 'white', 'black', 'white', 'white', 'black', 'white', 'black', 'white', 'white', 'black', 'white', 'black', 'white', 'black', 'white', 'white', 'black', 'white', 'black', 'white', 'white', 'black', 'white', 'black', 'white', 'black', 'white', 'white', 'black', 'black', ]
