da3f5a5fe99ea8be9fd4af39d102cbf8f01051f2
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-12 01:40:25 -0700

    space and time complexity added for piano problem

--- codewars/piano.js
+++ codewars/piano.js
@@ -3,7 +3,7 @@
 const expect = require('chai').expect;
 
 /*
-6kyu 
+6kyu
 The function you are going to write is not actually going to help you with your piano playing, but just explore one of the patterns you're experiencing: Given the number you stopped on, was it on a black key or a white key? For example, in the description of your piano exercise above, if you stopped at 5, your left thumb would be on the fifth key of the piano, which is black. Or if you stopped at 92, you would have gone all the way from keys 1 to 88 and then wrapped around, so that you would be on the fourth key, which is white.
 
 Your function will receive an integer between 1 and 10000 (maybe you think that in principle it would be cool to count up to, say, a billion, but considering how many years it would take it is just not possible) and return the string "black" or "white" -- here are a few more examples:
@@ -26,6 +26,9 @@ function blackOrWhiteKey(keyPressCount) {
     return piano[x - 1];
   }
 }
+
+// Space Complexity: O(N)
+// Time Complexity: O(1)
 
 describe('piano problem code wars', function() {
   describe('with input 1', function() {
