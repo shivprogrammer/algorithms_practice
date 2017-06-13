acfdc2c530b18b452f70d762772f6c0c844f9131
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-12 20:21:22 -0700

    mumbling code wars question scaffold established

---
+++ codewars/accum.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+/*
+Mumbling
+
+6 kyu
+
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+
+accum("abcd");    // "A-Bb-Ccc-Dddd"
+accum("RqaEzty"); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt");    // "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+*/
+
+function accum(s) {
+	// your code
+}
+
+describe('Mumbling function from code wars', function() {
+  describe('with input of ZpglnRxqenU', function() {
+    it('return Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu', done => {
+      expect(accum('ZpglnRxqenU')).to.equal('Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu');
+      done();
+    })
+  })
+
+  describe('with input of NyffsGeyylB', function() {
+    it('return Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu', done => {
+      expect(accum('NyffsGeyylB')).to.equal('N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb');
+      done();
+    })
+  })
+})
--- codewars/solved/6kyu/catkata.js
+++ codewars/solved/6kyu/catkata.js
@@ -3,6 +3,10 @@
 const expect = require('chai').expect;
 
 /*
+Cat Kata, Part 1
+
+6kyu
+
 In this, the first and simplest of a planned trilogy of cat katas :-), all you have to do is determine whether the distances between any visiting cats are large enough to make for a peaceful afternoon, or whether there is about to be an altercation someone will need to deal with by carrying one of them into the house or squirting them with water or what have you.
 As input your function will receive a list of strings representing the yard as a grid, and an integer representing the minimum distance needed to prevent problems (considering the cats' current states of sleepiness). A point with no cat in it will be represented by a "-" dash. Lou, Mustache Cat, and Raoul will be represented by an upper case L, M, and R respectively. At any particular time all three cats may be in the yard, or maybe two, one, or even none.
 If the number of cats in the yard is one or none, or if the distances between all cats are at least the minimum distance, your function should return True/true/TRUE (depending on what language you're using), but if there are two or three cats, and the distance between at least two of them is smaller than the minimum distance, your function should return False/false/FALSE.
