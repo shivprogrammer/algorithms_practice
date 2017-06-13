8cddda7f09ab3b2ec99712dc8cbac01b408f0ecd
shivprogrammer <shiv.unadkat@gmail.com>
2017-06-13 02:28:47 -0700

    wow, solved find-odd-int first shot with all tests passing on code wars

--- codewars/find-odd-int.js
+++ codewars/find-odd-int.js
@@ -3,6 +3,10 @@
 const expect = require('chai').expect;
 
 /*
+Find the Odd Int
+
+6kyu
+
 Given an array, find the int that appears an odd number of times.
 There will always be only one integer that appears an odd number of times.
 */
@@ -11,10 +15,21 @@ function findOdd(arr) {
   var numCount = new Map();
 
   for (let i = 0; i < arr.length; i++) {
-    
+    if (numCount.get(arr[i]) === 1) {
+      numCount.delete(arr[i]);
+      console.log(numCount);
+    }
+    else {
+      numCount.set(arr[i], 1);
+      console.log(numCount);
+    }
   }
-  return 0;
+
+  return numCount.keys().next().value;
 }
+
+// Time Complexity: O(N)
+// Space Complexity: O(N)
 
 describe('Find Odd Int Question Code Wars', function() {
   describe('with input [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]', function() {
@@ -26,7 +41,7 @@ describe('Find Odd Int Question Code Wars', function() {
 
   describe('with input [1,1,2,-2,5,2,4,4,-1,-2,5]', function() {
     it('should return -1', done => {
-      expect(findOdd([1,1,2,-2,5,2,4,4,-1,-2,5]).to.equal(-1);
+      expect(findOdd([1,1,2,-2,5,2,4,4,-1,-2,5])).to.equal(-1);
       done();
     })
   })
